425c45de5db9e0822304797997ca69e5f8a97a1a
jcobe21 <juan.cobena@alumnos.upm.es>
2025-12-12 18:22:28 +0100

    La habia liado pero creo que ya esta arreglado

--- PROYECTO/principal.c
+++ PROYECTO/principal.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 
 #include "principal.h"                                                          //
-#include "joystick.h"                                                           //
 
 /*******************************************************************************
  *                           MODULO PRINCIPAL..................................*
@@ -13,39 +12,35 @@
 #define MODO_REPRODUCCION 0x02
 #define MODO_HORA         0x03
 
-#define HORA              0x01
-#define TEMPERATURA       0x02
-#define JOYSTICK          0x03
-#define POT_2             0x04
+#define EV_JOY            0x01
+#define EV_POT           0x02
+#define EV_TEMP           0x03
+
+uint8_t modo_actual;
+uint8_t evento = 0X00;
 
 // HILO PRINCIPAL = TEST.C
 osThreadId_t tid_principal;
 //int Init_ThPrincipal (void);                                                  //en el .H
-
-int modo_actual;
 void ThPrincipal (void *argument);
 
-typedef struct {
-    uint8_t  source;  // ¿Quién lo envía? (FUENTE_JOYSTICK, FUENTE_TEMP...)
-    uint32_t msg;    // El dato (código de tecla, valor de temperatura, etc.)
-} mensaje_t;
-
+//COLAS DE OTROS MÓDULOS
 extern osMessageQueueId_t mid_MsgQueue_Joy;                                     //faltan los demás
+extern osMessageQueueId_t mid_MsgQueue_POT;
+extern osMessageQueueId_t mid_MsgQueueTemp;
+extern osMessageQueueId_t mid_MsgQueueMp3;                                      //FALTA mod MP3
 
-osStatus_t statusPrincipal;
-osMessageQueueId_t mid_MsgQueue_Principal;
-mensaje_t MsgQueue_Principal;
+//COLAS DEL H-PRINCIPAL ASOCIADAS A LOS MÓDULOS
+uint8_t msgJoy = 0x00;
+uint8_t msgPot = 0x00;
+float msgTemp = 0x00;
+t_comando msgMP3;                                                               //FALTA mod MP3
 
 int Init_ThPrincipal (void){ //Inicializamos hilo y cola
   
   tid_principal = osThreadNew(ThPrincipal, NULL, NULL);               //aquí le cambiamos el tamaño?
   if (tid_principal == NULL) {
     return(-1);
-  }
-  
-  mid_MsgQueue_Principal = osMessageQueueNew(16, sizeof(uint8_t), NULL);
-  if (mid_MsgQueue_Principal == NULL) {
-    return (-1);
   }
   
   return(0);
@@ -57,36 +52,56 @@ void ThPrincipal (void *argument){
   
   while(1){
     
-    statusPrincipal = osMessageQueueGet(mid_MsgQueue_Principal, &MsgQueue_Principal, NULL, osWaitForever);
+    if (osMessageQueueGet(mid_MsgQueue_Joy, &msgJoy, NULL, 10) == osOK) {
+          evento = EV_JOY;
+    }
+      // Si no hubo joystick, probar POT
+    else if (osMessageQueueGet(mid_MsgQueue_POT, &msgPot, NULL, 10) == osOK) {
+          evento = EV_POT;
+    }
+      // Si tampoco, probar TEMP
+    else if (osMessageQueueGet(mid_MsgQueueTemp, &msgTemp, NULL, 10) == osOK) {
+          evento = EV_TEMP;
+    }
     
-    switch(modo_actual){
-
-      case MODO_REPOSO :
-        //[añadir código]
-        if(MsgQueue_Principal.source == JOYSTICK){
-          if(MsgQueue_Principal.msg == 0x05){
-            modo_actual = MODO_REPRODUCCION;
-          }
-        } //faltan más cosas pero es la idea
-        break;
+    switch(modo_actual){                                                        //SWITCH PRINCIPAL
+    
+      case MODO_REPOSO :                                                        //MODO REPOSO
         
-      case MODO_REPRODUCCION :
-        if(MsgQueue_Principal == 0x05){
-          modo_actual = MODO_HORA;
-        } //faltan más cosas pero es la idea
-        break;
-        
-      case MODO_HORA :
         //[añadir código]
-        if(MsgQueue_Principal == 0x05){
-          modo_actual = MODO_REPOSO;
-        } //faltan más cosas pero es la idea
-        break;
+        
+        break;                                                                  //FIN modo reposo
+        
+      case MODO_REPRODUCCION :                                                  //MODO REPDRODUCCIÓN
+        
+        switch(evento){
+          case EV_JOY:
+            evento = 0x00; //borramos flag
+            if(msgJoy == 0x50){
+              //AÑADIR WAKE UP/SLEEP
+              //
+              modo_actual = MODO_HORA;
+            }
+            break;
+          case EV_POT:
+            
+            break;
+        }
+        
+        break;                                                                  //FIN modo reproducción
+        
+      case MODO_HORA :                                                          //MODO_HORA
+        
+        //[añadir código]
+        
+        break;                                                                  //FIN modo hora
         
       default:
+        modo_actual = MODO_REPOSO;
         break;
-
-    }
+     
+    } //FIN del switch principal
   
   } //fin del while(1)
+  
 }
--- PROYECTO/principal.h
+++ PROYECTO/principal.h
@@ -1,7 +1,11 @@
 #ifndef __PRINCIPAL_H
 #define __PRINCIPAL_H
 
+#include "joystick.h"                                                           //
+#include "temp.h"
+#include "pot.h"
+#include "mp3.h"                                                                //A partir de aquí faltan añadir módulos enteros
+
 int Init_ThPrincipal (void);
-int Init_MsgQueue_Principal(void);
 
 #endif
